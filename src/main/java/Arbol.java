bbc42e23228d561012e4369a506958ec3affb4b5
dnunezf <david.a.nunez27@outlook.com>
2024-10-16 22:43:18 -0600

    RECORRIDO EN AMPLITUD!

--- src/main/java/Arbol.java
+++ src/main/java/Arbol.java
@@ -2,10 +2,15 @@
 
 public class Arbol {
     // ATRIBUTO QUE CORRESPONDE A LA RAÍZ DEL ÁRBOL
-    private Nodo raiz;
+    private Nodo nodoRaiz;
+
+    // ATRIBUTO QUE ALMACENA LA ALTURA ACTUAL DEL ÁRBOL
+    private int alturaArbol;
+
+    // ARREGLO QUE CONTIENE LAS REPRESENTACIONES DE LOS NIVELES DEL ÁRBOL
+    private String[] nivelesArbol;
 
     // Constructor
-
     public Arbol() {
         // Creación de los animales
         Nodo culebra = new Nodo(new Animal("Culebra"));
@@ -28,10 +33,68 @@ public class Arbol {
         Nodo reptil = new Nodo(new Animal("Reptil"), mamifero, lagarto);
 
         // Asignar la raíz
-        raiz = new Nodo(new Animal("Ave"), reptil, aguila);
+        nodoRaiz = new Nodo(new Animal("Ave"), reptil, aguila);
     }
 
     // MÉTODO PARA INSERTAR LOS ANIMALES AL ÁRBOL
     public void insertar() {
+    }
+
+    // MÉTODO PARA CALCULAR Y OBTENER LA ALTURA DEL ÁRBOL
+    public int calcularAltura() {
+        alturaArbol = 0;
+        calcularAlturaRecursiva(nodoRaiz, 0);
+        return alturaArbol;
+    }
+
+    // MÉTODO RECURSIVO PRIVADO PARA CALCULAR LA ALTURA DEL ÁRBOL
+    // MÉTODO AUXILIAR
+    private void calcularAlturaRecursiva(Nodo nodoActual, int nivelActual) {
+        if (nodoActual != null) {
+            calcularAlturaRecursiva(nodoActual.getHijoIzquierdo(), nivelActual + 1);
+
+            if (nivelActual > alturaArbol) {
+                alturaArbol = nivelActual;
+            }
+
+            calcularAlturaRecursiva(nodoActual.getHijoDerecho(), nivelActual + 1);
+        }
+    }
+
+    // MÉTODO PARA IMPRIMIR LOS NIVELES DEL ÁRBOL
+    public void imprimirNiveles()
+    {
+        // Primero, calculamos la altura del árbol
+        alturaArbol = calcularAltura();
+
+        // Inicializa el arreglo de niveles
+        nivelesArbol = new String[alturaArbol + 1];
+
+        // Inicializa cada nivel como una cadena vacía
+        for (int i = 0; i < nivelesArbol.length; i++)
+        {
+            nivelesArbol[i] = "";
+        }
+
+        // Llama al método recursivo, para llenar los niveles
+        llenarNivelesRecursivamente(nodoRaiz, 0);
+
+        // Imprime los niveles
+        for (int i = 0; i < nivelesArbol.length; i++) {
+            System.out.println("Nivel " + i + ": " + nivelesArbol[i]);
+        }
+    }
+
+    // MÉTODO RECURSIVO PRIVADO PARA LLENAR EL ARREGLO DE NIVELES DEL ÁRBO
+    // MÉTODO AUXILIAR
+    private void llenarNivelesRecursivamente(Nodo nodoActual, int nivelActual) {
+        if (nodoActual != null) {
+            // Agrega el nombre del animal al nivel actual
+            nivelesArbol[nivelActual] += nodoActual.getAnimal().getNombre() + " ";
+
+            // Continuar con los nodos hijos
+            llenarNivelesRecursivamente(nodoActual.getHijoIzquierdo(), nivelActual + 1);
+            llenarNivelesRecursivamente(nodoActual.getHijoDerecho(), nivelActual + 1);
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,15 @@
-/*MAIN*/
+/*MAIN
+*
+* REFERENCIAS (BIBLIOGRAFÍAS):
+* (2019, 04 abril). JAVA - Recorrido de árboles binarios por niveles | Sergio Ramirez
+* https://youtu.be/RyKvFhoP200?si=1iKV8XVHpqXaKMhe
+* */
 
-public class Main {
+public class Main
+{
     public static void main(String[] args) {
         Arbol arbol = new Arbol();
+
+        arbol.imprimirNiveles();
     }
 }
